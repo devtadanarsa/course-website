25b48f62901b80023771a4722b0bb6698d1ff977
Devtaaa <devtadanarsa69@gmail.com>
2023-12-19 16:43:27 +0800

    make detail table in course

--- frontend/src/pages/admin/[id].js
+++ frontend/src/pages/admin/[id].js
@@ -3,12 +3,13 @@ import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
 import {Button} from "@nextui-org/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
+import { faFloppyDisk, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function EditCourse(){
     const router = useRouter();
 
     const [courseData, setCourseData] = useState(null);
+    const [chapterData, setChapterData] = useState(null);
     const [courseId, setCourseId] = useState(null);
 
     const [courseTitle, setCourseTitle] = useState("");
@@ -24,6 +25,7 @@ export default function EditCourse(){
     }, [router.query.id]);
     
     const url = `http://localhost:8000/lessons/${courseId}`;
+    const chapterUrl = `http://localhost:8000/lessons/${courseId}/1`;
 
     const fetchInfo = () => {
         if(courseId){
@@ -36,6 +38,16 @@ export default function EditCourse(){
                     setDetailedDescription(data.length > 0 ? data[0].detailed_description : "");
                     setEstimatedTotalTime(data.length > 0 ? data[0].total_time : "");
                     setDifficulty(data.length > 0 ? data[0].difficulty : "");
+                })
+        }
+    }
+
+    const chapterFetchInfo = () => {
+        if(courseId){
+            return fetch(chapterUrl)
+                .then((res) => res.json())
+                .then((data) => {
+                    setChapterData(data);
                 })
         }
     }
@@ -83,29 +95,77 @@ export default function EditCourse(){
 
     useEffect(() => {
         fetchInfo();
+        chapterFetchInfo();
     }, [courseId]);
 
 
     return(
-        <div className="mx-24">
-            {courseData && (
-                <>
-                    <FormField name="courseTitle" label="Course Title" type="text" placeholder="Course Title" value={courseTitle || ""} onChange={handleInputChange}/>
-                    <FormField name="mainDescription" label="Main Description" type="text" placeholder="Main Description" value={mainDescripton || ""} onChange={handleInputChange}/>
-                    <FormField name="detailedDescription" label="Detailed Description" type="text" placeholder="Detailed Description" value={detailedDescription || ""} onChange={handleInputChange}/>
-                    <FormField name="estimatedTotalTime" label="Estimated Total Time" type="text" placeholder="Estimated Total TIme" value={estimatedTotalTime || ""} onChange={handleInputChange}/>
-                    <FormField label="Difficulty" type="select" placeholder="Difficulty" name="difficulty" value={difficulty} onChange={handleInputChange}>
-                        {["Pemula", "Menengah", "Mahir", "Profesional"].map((option) => {
-                            return(
-                            <option key={option} value={option} selected={option === difficulty}>
-                                {option}
-                            </option>
-                            )
-                        })}
-                    </FormField>
-                    <Button radius="sm" color="success" className="mt-5 px-6 text-white" startContent={<FontAwesomeIcon icon={faFloppyDisk} style={{color: "#ffffff",}}/>} onClick={handleEditCourse}>Save</Button>
-                </>
-            )}
-        </div>
+        <main className="mx-24">
+            <section>
+                <h1 className="text-[30px] font-semibold mt-10 mb-6">Edit Course</h1>
+                <div>
+                    {courseData && (
+                        <>
+                            <FormField name="courseTitle" label="Course Title" type="text" placeholder="Course Title" value={courseTitle || ""} onChange={handleInputChange}/>
+                            <FormField name="mainDescription" label="Main Description" type="text" placeholder="Main Description" value={mainDescripton || ""} onChange={handleInputChange}/>
+                            <FormField name="detailedDescription" label="Detailed Description" type="text" placeholder="Detailed Description" value={detailedDescription || ""} onChange={handleInputChange}/>
+                            <FormField name="estimatedTotalTime" label="Estimated Total Time" type="text" placeholder="Estimated Total TIme" value={estimatedTotalTime || ""} onChange={handleInputChange}/>
+                            <FormField label="Difficulty" type="select" placeholder="Difficulty" name="difficulty" value={difficulty} onChange={handleInputChange}>
+                                {["Pemula", "Menengah", "Mahir", "Profesional"].map((option) => {
+                                    return(
+                                    <option key={option} value={option} selected={option === difficulty}>
+                                        {option}
+                                    </option>
+                                    )
+                                })}
+                            </FormField>
+                            <Button radius="sm" color="success" className="mt-5 px-6 text-white" startContent={<FontAwesomeIcon icon={faFloppyDisk} style={{color: "#ffffff",}}/>} onClick={handleEditCourse}>Save</Button>
+                        </>
+                    )}
+                </div>
+            </section>
+            <section>
+                <h1 className="text-[30px] font-semibold mt-10 mb-6">Course Chapter</h1>
+                {chapterData && (
+                    <>
+                        <table className="w-full">
+                            {chapterData.map((chapter) => {
+                                return(
+                                    <>
+                                        <thead>
+                                            <tr>
+                                                <th className="border py-5 text-[20px] text-left pl-4" colSpan="4">Chapter : {chapter.chapter_title}</th>
+                                            </tr>
+                                            <tr>
+                                                <th className="border py-2">No</th>
+                                                <th className="border">Content Name</th>
+                                                <th className="border">Video Link</th>
+                                                <th className="border">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {chapter.content.map((data) => {
+                                                return(
+                                                    <tr key={data.id}>
+                                                        <td className="text-center border">{data.id}</td>
+                                                        <td className="border text-left pl-3">{data.name}</td>
+                                                        <td className="border text-left pl-3">{data.link}</td>
+                                                        <td className="border text-center py-2">
+                                                            <div>
+                                                            <Button color="danger" className="text-white" isIconOnly startContent={<FontAwesomeIcon icon={faTrash} style={{color: "#ffffff",}}/>} onClick={() => deleteCourse(data.course_id)}/>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            })}
+                                        </tbody>
+                                    </>
+                                )
+                            })}
+                        </table>
+                    </>
+                )}
+            </section>
+        </main>
     )
 }
